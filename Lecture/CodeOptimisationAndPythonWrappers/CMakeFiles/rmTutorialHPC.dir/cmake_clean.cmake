104a26dc92f6def4a3a70cbbac896361f4e6d8d6
Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2019-03-18 17:18:49 +0100

    C++ lecture has been moved to pages .io

--- Lecture/CodeOptimisationAndPythonWrappers/CMakeFiles/rmTutorialHPC.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rmTutorialHPC"
-  "../rmTutorial.txt"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rmTutorialHPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Lecture/CodeOptimisationAndPythonWrappers/CMakeFiles/tutorialHPC.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tutorialHPC"
-  "index.html"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tutorialHPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
