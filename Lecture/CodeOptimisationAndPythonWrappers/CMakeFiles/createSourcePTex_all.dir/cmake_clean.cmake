bfeee5cece0e18f42cd2f48a443e8efef2918c42
Pierre Aubert <pierre.aubert@lapp.in2p3.fr>
2019-03-18 16:53:39 +0100

    Add lecture for code optimisation and python wrappers

---
+++ Lecture/CodeOptimisationAndPythonWrappers/CMakeFiles/createSourcePTex_all.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/createSourcePTex_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lecture/CodeOptimisationAndPythonWrappers/CMakeFiles/rmTutorialHPC.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rmTutorialHPC"
+  "../rmTutorial.txt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/rmTutorialHPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
