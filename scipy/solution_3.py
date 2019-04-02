aadffa870cd8fcb078f812a179f6c0228950308e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-04-02 14:26:10 +0200

    Add script to create alien signal

--- scipy/solution_3.py
+++ scipy/solution_3.py
@@ -1,14 +1,22 @@
-# The clock speed gives us the points in time we need to sample the signal.
-clock = np.arange(clock_speed, len(alien_signal), sample_length)
+# let's sample the signal in the middle of each period
+clock = np.arange(sample_length // 2, len(alien_signal), sample_length)
 
 plt.figure()
 plt.plot(alien_signal)
 plt.plot(clock, alien_signal[clock], 'ro')
 
 
-#Now cross correlate the signal with the pulse shape. We know the pulse shape and its length.
+# Now cross correlate the signal with the pulse shape.
+# We know the pulse shape and its length.
 
-corr = signal.correlate(alien_signal, signal.boxcar(sample_length), mode='same') /sample_length
+corr = signal.correlate(
+    alien_signal,
+    signal.boxcar(sample_length),
+    mode='same'
+) /sample_length
+
+
+# plot stuff
 plt.figure()
 plt.plot(corr)
 plt.plot(clock, corr[clock], 'ro')
@@ -19,4 +27,4 @@ plt.axhline(0.5, ls='--', color='gray')
 a = np.where(corr[clock] > 0.5, 1, 0)
 s = bytearray(np.packbits(a)).decode('ascii')
 
-print(f'Decoded Alien Message is:  "{s}" ')
+print(f'Decoded Alien Message is: "{s}" ')
