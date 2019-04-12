b55d2ffd83a7f288d76732e79a7e1c553ae82a42
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-04-12 15:27:25 +0200

    Use abspath

--- cpp_intro/6_python/solution/setup.py
+++ cpp_intro/6_python/solution/setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+import os
 
 ext_modules = [
     Extension(
         'fib_wrapper',
         sources=['fib_wrapper.pyx'],
         libraries=['fibonacci'],
-        runtime_library_dirs=['build'],
+        runtime_library_dirs=[os.path.abspath('build')],
         include_dirs=['include'],
         library_dirs=['build'],
     )
@@ -15,5 +16,6 @@ ext_modules = [
 
 setup(
     name='fib_wrapper',
+    version='0.0.1',
     ext_modules=cythonize(ext_modules)
 )
